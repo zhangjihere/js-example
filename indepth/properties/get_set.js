64cb10cbae3b96616a8b6b0147d9a8ca836ca1fd
Ji Zhang <ji.zhang@samsung.com>
2018-07-19 18:45:09 +0800

    update get_set property

--- indepth/properties/get_set.js
+++ indepth/properties/get_set.js
@@ -15,3 +15,18 @@ console.log(bb.hasOwnProperty("toString")); //true
 // check if a property is in prototype chain
 //   key in obj
 //   Reflect.has(obj, key)
+// Loop-Thru(Enumerate) Properties
+// for (let k in o) {...};     //protochain yes,enumerable only yes, include symbol_key no, include string_key yes
+// for (let v of iterable) {.};//N/A,  		N/A,			N/A,			N/A
+
+console.log("list Object property:");
+const o = {"k1": 20, "k2": 30};
+for (let k in o) {
+  console.log(o[k]);
+}
+console.log();
+for (let k of Object.keys(o)) {
+  console.log(o[k]);
+}
+
+
