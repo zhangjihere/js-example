e5be1c81ef5c661a31ea283259a943dfe5b9c858
Ji Zhang <ji.zhang@samsung.com>
2018-07-19 18:57:08 +0800

    add property_attributes.js

---
+++ indepth/properties/property_attributes.js
@@ -0,0 +1,20 @@
+// Property Attributes
+// value: the property's value
+// writable: when true, the property's value can be changed.
+// enumerable: when true, the property shows in some loop constructs, such as for-in loop.
+// configurable: If false, attempts to delete the property, change the property to be an accessor property, or change its attributes (other than [[Value]], or changing [[Writable]] to false) will fail.
+
+// Object.getOwnPropertyDescriptor(obj, key)
+// Reflect.getOwnPropertyDesciptor(obj, key)
+// 
+// check property's enumeratable attribute
+//   obj.propertyIsEnumeratable(p)
+//   Object.prototype.propertyIsEnumerable
+//   
+//   
+//   Object.defineProperty
+//   Object.definePreperties
+
+
+
+
